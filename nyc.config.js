6e2306b7b92075c2bc01b8b1af427fbb7f41183b
Florian Mäder <florian@maederbiel.ch>
2020-06-02 22:17:30 +0200

    refactor(test): move nyc config to file
    And add check-coverage and additional reporter.

---
+++ nyc.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+	'reporter': [
+		'html',
+		'text-summary',
+	],
+
+	'check-coverage': true,
+
+	'branches':   100,
+	'lines':      100,
+	'functions':  100,
+	'statements': 100,
+};
